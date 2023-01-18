22b3c68dfc6a2d49ffd376c139ea52ee81c70248
jimatth <jimatth121@gmail.com>
2023-01-18 18:44:09 +0100

    Updated

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.data.title);
+  // const [title, setTitle] = useState(props.data.title);
 
-  const clickHandler = () => {
-    setTitle("Update");
+  // const clickHandler = () => {
+  //   setTitle("Update");
 
-    setTimeout(() => {
-      console.log(title);
-    }, 3000);
-  };
+  //   setTimeout(() => {
+  //     console.log(title);
+  //   }, 3000);
+  // };
 
   return (
     <div className="flex justify-between bg-gray-800 mb-4 py-2 items-center rounded-md px-4 w-full">
       <div className="flex gap-x-4 items-center">
         <ExpenseDate date={props.data.date} />
-        <div className="text-2xl text-white">{title}</div>
+        <div className="text-2xl text-white">{props.data.title}</div>
       </div>
 
       <div className="py-2 bg-blue-900 rounded-md border-[1px] border-[#eee] px-10 text-white">
